49c7f34280d0ce995e90d448ec4100246d1f691d
Luciano Alves <luas10c@gmail.com>
2024-02-13 19:14:24 -0300

    chore: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+const config = {
+  root: true,
+  env: {
+    browser: true,
+    es2023: true,
+    jest: true
+  },
+  extends: [
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: '14'
+  },
+  overrides: [
+    {
+      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching testing files!
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:jest/recommended', 'plugin:testing-library/react'],
+      plugins: ['jest']
+    }
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'tailwindcss/no-custom-classname': 'off'
+  }
+}
+
+module.exports = config
