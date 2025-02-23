43e6fb2f2c13b18fdf25a72cae76838deb98c9a9
Luciano Alves <luas10c@gmail.com>
2025-02-23 11:41:32 -0300

    chore: first commit

---
+++ src/screens/connection.tsx
@@ -0,0 +1,9 @@
+export function Connection() {
+  return (
+    <section className="text-bunker-100 relative flex flex-1 flex-col">
+      <div>
+        <h3 className="text-white">.connection</h3>
+      </div>
+    </section>
+  )
+}
---
+++ src/screens/preferences.tsx
@@ -0,0 +1,7 @@
+export function Preferences() {
+  return (
+    <section className="text-bunker-100 relative flex flex-1 flex-col">
+      <div className="text-white">.preferences</div>
+    </section>
+  )
+}
