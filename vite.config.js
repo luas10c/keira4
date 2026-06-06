b63ba17d86bc4601d43ca1f27da5d59d8c8a87a5
Luciano Alves <luas10c@gmail.com>
2026-06-06 06:25:56 -0300

    chore(config): remove unused imports

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import svgr from '@svgr/rollup'
 import tailwindcss from '@tailwindcss/vite'
